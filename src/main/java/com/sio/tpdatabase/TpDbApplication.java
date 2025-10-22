d539981b1b00c713cd1509c3c1b16c2bea95dd84
JPechberty <j.pechberty@gmail.com>
2025-10-22 10:06:47 +0200

    replace by HIbernate INstallation

--- src/main/java/com/sio/tpdatabase/TpDbApplication.java
+++ src/main/java/com/sio/tpdatabase/TpDbApplication.java
@@ -4,16 +4,38 @@ import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.context.ConfigurableApplicationContext;
 
 import java.io.IOException;
 
+@SpringBootApplication
 public class TpDbApplication extends Application {
+
+    private ConfigurableApplicationContext springContext;
+
+    @Override
+    public void init() throws Exception {
+        springContext = new SpringApplicationBuilder(TpDbApplication.class).run();
+    }
+
     @Override
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(TpDbApplication.class.getResource("hello-view.fxml"));
+        fxmlLoader.setControllerFactory(springContext::getBean);
         Scene scene = new Scene(fxmlLoader.load(), 320, 240);
         stage.setTitle("Hello!");
         stage.setScene(scene);
         stage.show();
+    }
+
+    @Override
+    public void stop() throws Exception {
+        springContext.close();
+    }
+
+    public static void main(String[] args) {
+        launch(args);
     }
 }
--- src/main/java/com/sio/tpdatabase/TpDbController.java
+++ src/main/java/com/sio/tpdatabase/TpDbController.java
@@ -2,7 +2,9 @@ package com.sio.tpdatabase;
 
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
+import org.springframework.stereotype.Component;
 
+@Component
 public class TpDbController {
     @FXML
     private Label welcomeText;
